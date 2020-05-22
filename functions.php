fbacc4ebc63418fc9f4f32ab1d074105d74a8c71
Jibundeyare <contact@jibundeyare.com>
2020-05-22 12:54:24 +0200

    Ajout de code

--- functions.php
+++ functions.php
@@ -15,10 +15,10 @@ function addition($a, $b) {
 }
 
 // appel d'une fonction
-$somme = addition(123, 42);
+$resultat = addition(123, 42);
 
 echo '<pre>';
-var_dump($somme);
+var_dump($resultat);
 echo '</pre>';
 echo '<br>';
 
@@ -28,7 +28,12 @@ function addition2(float $a, float $b): float {
 }
 
 // l'appel de la fonction est identique avec ou sans type hinting
-$somme = addition2(123, 42);
+$resultat = addition2(123, 42);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
 
 // déclaration d'une fonction anonyme avec type hinting
 $multiplication = function(float $a, float $b): float {
@@ -36,4 +41,65 @@ $multiplication = function(float $a, float $b): float {
 };
 
 // l'appel d'une fonction anonyme
-$produit = $multiplication(2, 123);
+$resultat = $multiplication(2, 123);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
+
+// exemple de closure
+$b = 2;
+// définition d'une closure
+// le mot clé "use" permet de figer la valeur d'une variable dans une fonction
+$somme2 = function($a) use ($b) {
+    return $a + $b;
+};
+// appel d'une closure
+$resultat = $somme2(100);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
+
+$b = 10;
+// définition d'une closure
+$somme10 = function($a) use ($b) {
+    return $a + $b;
+};
+// appel d'une closure
+$resultat = $somme10(100);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
+
+// exemple de factory d'une closure
+function sumFactory($b) {
+    // définition d'une closure
+    return function($a) use ($b) {
+        return $a + $b;
+    };
+}
+
+// fabrication d'une closure
+$somme2 = sumFactory(2);
+// appel d'une closure
+$resultat = $somme2(100);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
+
+// fabrication d'une closure
+$somme10 = sumFactory(10);
+// appel d'une closure
+$resultat = $somme10(100);
+
+echo '<pre>';
+var_dump($resultat);
+echo '</pre>';
+echo '<br>';
